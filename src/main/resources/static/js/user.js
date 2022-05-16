5968576b634b22661913091ae063f0c379de682f
ggoma0626 <zxcqwe36@naver.com>
2022-05-16 11:19:04 +0900

    table

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,76 +1,79 @@
-$(document).ready(function(){
+// javascript -> jquery
+// java -> kotlin
+
+$(document).ready(function () {
     getUser();
-    $('#btnSignup').click(function() {
-       var name = $('#name').val();
-       var age = $('#age').val();
-       var phone_number = $('#phone_number').val();
+    $('#btnSignup').click(function () {
+        var name = $('#name').val();
+        var age = $('#age').val();
+        var phone_number = $('#phone_number').val();
 
-       var query = {
-           name: name, // key : id, value : adminId
-           age: age,
-           phoneNumber: phone_number
-       };
+        var query = {
+            name: name, // key : id, value : adminId
+            age: age,
+            phoneNumber: phone_number
+        };
 
-       //통신하는 부분
+        //통신하는 부분 (비동기통신법)
 
-       $.ajax({
-           type: "POST",
-           url: "http://192.168.0.232:8080/signup",
-           data: query,
-           contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        $.ajax({
+            type: "POST",
+            url: "http://192.168.0.232:8080/signup",
+            data: query,
+            contentType: "application/x-www-form-urlencoded; charset=UTF-8",
 
-           success: function (res) {
-               alert(res);
-               getUser();
-           }
-       });
+            success: function (res) {
+                alert(res);
+                getUser();
+            }
+        });
     });
 
-    $('#btnSearch').click(function() {
-    var inputname = prompt("찾으실 회원의 이름을 입력하세요");
+    $('#btnSearch').click(function () {
+        var inputname = prompt("찾으실 회원의 이름을 입력하세요");
 
-      var query = {
-          name: inputname, // key : id, value : adminId
-      };
+        var query = {
+            name: inputname, // key : id, value : adminId
+        };
 
-      $.ajax({
-          type: "GET",
-          url: "http://192.168.0.232:8080/search",
-          data: query,
-          contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        $.ajax({
+            type: "GET",
+            url: "http://192.168.0.232:8080/search",
+            data: query,
+            contentType: "application/x-www-form-urlencoded; charset=UTF-8",
 
-          success: function (res) {
+            success: function (res) {
 
-             $('#tbody').html("");
-             var list = res;
-             var listLen = list.length;
-             for (var i = 0; i < listLen; i++) {
-                 var str = "<tr>" +
-                     "<td>" + list[i].id + "</td>" +
-                     "<td>" + list[i].name + "</td>" +
-                     "<td>" + list[i].age + "</td>" +
-                     "<td>" + list[i].phoneNumber + "</td>" +
-                     "</tr>";
-                 $('#tbody').append(str);
+                $('#tbody').html("");
+                var list = res;
+                var listLen = list.length;
+                for (var i = 0; i < listLen; i++) {
+                    var str = "<tr>" +
+                        "<td>" + list[i].id + "</td>" +
+                        "<td>" + list[i].name + "</td>" +
+                        "<td>" + list[i].age + "</td>" +
+                        "<td>" + list[i].phoneNumber + "</td>" +
+                        "</tr>";
+                    $('#tbody').append(str);
 
-             }
+                }
 
-          }
-      });
+            }
+        });
     });
 
 
-    $('#btnDelete').click(function() {
+    $('#btnDelete').click(function () {
 
-    var deleteUser = prompt("삭제하실 회원의 ID를 입력하세요");
+        var deleteUser = prompt("삭제하실 회원의 ID를 입력하세요");
 
-    var query = {
-        id: deleteUser, // key : id, value : adminId
-    };
+        var query = {
+            id: deleteUser, // key : id, value : adminId
+        };
 
 
 
-     //통신하는 부분
+        //통신하는 부분
 
         $.ajax({
             type: "POST",
@@ -85,43 +88,76 @@ $(document).ready(function(){
         });
     });
 
-            $('#btnUserAll').click(function() {
-                //통신하는 부분
+    $('#btnUserAll').click(function () {
+        //통신하는 부분
 
-                $.ajax({
-                    type: "GET",
-                    url: "http://192.168.0.232:8080/users",
-                    contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        $.ajax({
+            type: "GET",
+            url: "http://192.168.0.232:8080/users",
+            contentType: "application/x-www-form-urlencoded; charset=UTF-8",
 
-                    success: function () {
-                        getUser();
-                    }
-                });
+            success: function () {
+                getUser();
+            }
+        });
+    });
+
+    $('#btnUpdate0').click(function () {
+    alert("0000");
+        var inputId = $("#id0").html();
+
+        var inputName = $('#name0').html();
+
+        $("#name").val(inputName);
+
+        $("#age").val(inputAge);
+
+        $("#phone_number").val(inputPhone);
+
+
+
+            var query = {
+                name: inputname, // key : id, value : adminId
+            };
+
+            $.ajax({
+                type: "GET",
+                url: "http://192.168.0.232:8080/update",
+                data: query,
+                contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+
+                success: function (res) {
+
+
+                }
             });
+    });
 
 
-   function getUser() {
-       $.ajax({
-           type: "GET",
-           url: "http://192.168.0.232:8080/users",
-           contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+    function getUser() {
+        $.ajax({
+            type: "GET",
+            url: "http://192.168.0.232:8080/users",
+            contentType: "application/x-www-form-urlencoded; charset=UTF-8",
 
-           success: function (json) {
-               $('#tbody').html("");
-               var list = json;
-               var listLen = list.length;
-               for (var i = 0; i < listLen; i++) {
-                   var str = "<tr>" +
-                       "<td>" + list[i].id + "</td>" +
-                       "<td>" + list[i].name + "</td>" +
-                       "<td>" + list[i].age + "</td>" +
-                       "<td>" + list[i].phoneNumber + "</td>" +
-                       "</tr>";
-                   $('#tbody').append(str);
+            success: function (json) {
+                $('#tbody').html("");
+                var list = json;
+                var listLen = list.length;
+                for (var i = 0; i < listLen; i++) {
 
-               }
+                    var str = "<tr>" +
+                        "<td id='id" + i + "'>" + list[i].id + "</td>" +
+                        "<td id='name" + i + "'>" + list[i].name + "</td>" +
+                        "<td id='age" + i + "'>" + list[i].age + "</td>" +
+                        "<td id='phone_number" + i + "'>" + list[i].phoneNumber + "</td>" +
+                        "<td><button type='button' class='btn btn-primary' id='btnUpdate" + i + "'>수정</button></td>"
+                        "</tr>";
+                    $('#tbody').append(str);
 
-           }
-       });
-   }
+                }
+                alert($('#tbody').html());
+            }
+        });
+    }
 })
